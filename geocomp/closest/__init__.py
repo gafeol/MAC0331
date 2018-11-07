33790fd863fe1f8f91123533045da76a17ec725c
Gabriel Fernandes <gabfeol@gmail.com>
2018-11-06 23:41:30 -0200

    Primeira versao line sweep

--- geocomp/closest/__init__.py
+++ geocomp/closest/__init__.py
@@ -5,11 +5,14 @@ Dado um conjunto de pontos S, determinar dois cuja distancia entre eles seja min
 
 Algoritmos disponveis:
 - Fora bruta
+- Line Sweep
 """
 from . import brute
+from . import sweep
 
 children = [
-	[ 'brute', 'Brute', 'Forca Bruta' ]
+	[ 'brute', 'Brute', 'Forca Bruta' ],
+	[ 'sweep', 'Sweep', 'Line Sweep'  ]
 ]
 
 __all__ = [a[0] for a in children]
---
+++ geocomp/closest/sweep.py
@@ -0,0 +1,77 @@
+#!/usr/bin/env python
+"""Algoritmo de line sweep"""
+
+from geocomp.common.segment import Segment
+from geocomp.common.point import Point
+from geocomp.common import control
+from geocomp.common.guiprim import *
+from geocomp.config import *
+import math
+
+
+List = []
+
+def comparax(x, y):
+	return x.x - y.x
+
+def Sweep (l):
+	"Algoritmo que usa line sweep para encontrar o par de pontos mais proximo"
+
+	if len (l) < 2: return None
+
+	List.clear()
+
+# Ordena por x os pontos recebidos
+	l.sort(key=lambda o: o.x)
+	
+	closest = float("inf")
+	a = b = None
+	id = None
+
+	dt = {}
+
+	for i in range (len(l)):
+		for j in List:
+			# Checa o segmento de y em O(n) 
+            #TODO: achar segmento em log(n)
+			if(abs(l[j].y - l[i].y) < math.sqrt(closest)):
+				dist = dist2 (l[i], l[j])
+				if dist < closest:
+					control.freeze_update ()
+					if a != None: a.unhilight (hia)
+					if b != None: b.unhilight (hib)
+					if id != None: control.plot_delete (id)
+
+					closest = dist
+					a = l[i]
+					b = l[j]
+
+					hia = a.hilight ()
+					hib = b.hilight ()
+					id = a.lineto (b)
+					control.thaw_update ()
+					control.update ()
+
+		# Remocao do line sweep em O(n)
+        # TODO: Realizar remocao em O(lgn)
+		for j in List:
+			if math.sqrt(closest) < l[i].x - l[j].x:
+				List.remove(j)
+				l[j].unhilight(dt[l[j]])
+
+		List.append(i)
+		hi = l[i].hilight(COLOR_ALT1)
+		dt[l[i]] = hi
+
+		control.thaw_update()
+		control.update()
+	
+	for j in List:
+		l[j].unhilight(dt[l[j]])
+
+	control.thaw_update()
+	control.update()
+
+	ret = Segment (a, b)
+	ret.extra_info = 'distancia: %.2f'%math.sqrt (dist2 (a, b))
+	return ret
